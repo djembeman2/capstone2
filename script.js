c0719abd3c8b0a5113c0d77f7ecbfd43f15f646d
djembeman2 <52840818+djembeman2@users.noreply.github.com>
2024-01-05 14:21:35 -0600

    added a button in java as well both have alerts

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function onButtonClick(){
-  alert('Making Music');
+  alert('We Making Music');
 }
 
 
@@ -7,4 +7,11 @@ const button = document.querySelector('button')
 button.addEventListener('click', onButtonClick);
 
 
+const newButton = document.createElement('button');
+newButton.textContent = 'New Music!';
+document.body.appendChild(newButton);
 
+
+newButton.addEventListener('click', () => {
+  alert('Haus Of Percussion is on itunes!');
+});
