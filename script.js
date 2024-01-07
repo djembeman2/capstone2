88790c29beb1f8e0b6b4fecfadb3cad9f2b1b24b
djembeman2 <52840818+djembeman2@users.noreply.github.com>
2024-01-06 20:56:41 -0600

    code work

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function onButtonClick(){
   alert('We Making Music');
   const musicTitle= document.querySelector('#clover-h2');
-  musicTitle.style.backgroundColor="red";
+  musicTitle.style.backgroundColor="lightblue";
 }
 
 
@@ -17,3 +17,18 @@ document.body.appendChild(newButton);
 newButton.addEventListener('click', () => {
   alert('Haus Of Percussion is on itunes!');
 });
+
+alert ("Want to hear a joke?");
+
+confirm ("where did the compter start working?");
+
+let g = prompt("would you like to answer...");
+
+let guess = parseInt(g);
+
+if(guess === 7){
+  alert("the Motherboard");
+} else {
+  alert("try agian!");
+}
+alert("We Love coding");
