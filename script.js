24cf25a675e7d9b840abaf22f8e970c57275d3f1
djembeman2 <52840818+djembeman2@users.noreply.github.com>
2024-01-10 18:03:29 -0600

    code displays no error how ever the tags dont change as insructed

--- script.js
+++ script.js
@@ -18,9 +18,56 @@ let g = prompt("would you like to answer...");
 
 let guess = parseInt(g);
 
-if(guess === 7){
+if(7 === 7){
   alert("the Motherboard");
 } else {
   alert("try agian!");
 }
 alert("We Love coding");
+
+colorTheme = {
+  liveColor: "orange",
+  liveBackgroundColor: "brown",
+  inColor: "red",
+  inBackgroundColor: "blue",
+  musicColor:"red",
+  musicBackgroundColor:"orange",
+  alwaysColor: "brown",
+  alwaysBackgroundColor:"orange"
+};
+
+function setLiveStyle(){
+  liveHeader = document.querySelector("#live");
+  liveHeader.style.color = colorTheme.liveColor;
+  liveHeader.style.backgroundColor = colorTheme.liveBackgroundColor;
+}
+
+function setInStyle() {
+  inHeader = $("#inut");
+  inHeader.style.color = colorTheme.inColor;
+  inHeader.style.backgroundColor = colorTheme.inBackgroundColor;
+}
+
+
+function setMusicStyle() {
+  musicHeader = $("#mus");
+  musicHeader.style.color = colorTheme.musicColor;
+  musicHeader.style.backgroundColor = colorTheme.musicBackgroundColor;
+}
+
+
+function setAlwaysStyle() {
+  alwaysHeader = $("#alw");
+  alwaysHeader.style.color = colorTheme.alwaysColor;
+  alwaysHeader.style.backgroundColor = colorTheme.alwaysBackgroundColor;
+}
+
+function setAll() {
+  setLiveStyle();
+  setInStyle();
+  setMusicStyle();
+  setAlwaysStyle();
+}
+
+let chanButton = ("#chan-btn");
+chanButton.onclick = setAll;
